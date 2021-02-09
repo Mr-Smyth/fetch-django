20835f871df2c08584c88ec7d6bcb13482b37b6f
Eamonn Smyth <e.smyth75@gmail.com>
2021-02-09 23:10:53 +0000

    Updated all code to handle fetch in django

--- fetch_django/settings.py
+++ fetch_django/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'fetch_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        ''NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
